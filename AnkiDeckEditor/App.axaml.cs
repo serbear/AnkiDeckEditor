48146d47149520d12a8048bdc85fd1dde8becc39
Sergei Markov <smarkov.dev@gmail.com>
2024-08-13 10:59:22 +0300

    Dependency injection implementation.

--- AnkiDeckEditor/App.axaml.cs
+++ AnkiDeckEditor/App.axaml.cs
@@ -3,15 +3,36 @@ using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
 using AnkiDeckEditor.ViewModels;
 using AnkiDeckEditor.Views;
+using HanumanInstitute.MvvmDialogs;
+using HanumanInstitute.MvvmDialogs.Avalonia;
+using Microsoft.Extensions.Logging;
+using Splat;
 
 namespace AnkiDeckEditor;
 
 // ReSharper disable once PartialTypeWithSinglePart
 public partial class App : Application
 {
+    public static MainWindowViewModel MainWindow => Locator.Current.GetService<MainWindowViewModel>()!;
+
     public override void Initialize()
     {
         AvaloniaXamlLoader.Load(this);
+
+        // ReSharper disable once UnusedParameter.Local
+        var loggerFactory = LoggerFactory.Create(builder => builder.AddFilter(logLevel => true).AddDebug());
+
+        var build = Locator.CurrentMutable;
+        build.RegisterLazySingleton(() => (IDialogService)new DialogService(
+            new DialogManager(
+                new ViewLocator(),
+                logger: loggerFactory.CreateLogger<DialogManager>(),
+                dialogFactory: new DialogFactory().AddFluent()),
+            x => Locator.Current.GetService(x)));
+
+        // Register DI.
+        SplatRegistrations.Register<MainWindowViewModel>();
+        SplatRegistrations.SetupIOC();
     }
 
     public override void OnFrameworkInitializationCompleted()
--- AnkiDeckEditor/ViewLocator.cs
+++ AnkiDeckEditor/ViewLocator.cs
@@ -2,28 +2,37 @@ using System;
 using Avalonia.Controls;
 using Avalonia.Controls.Templates;
 using AnkiDeckEditor.ViewModels;
+using AnkiDeckEditor.Views;
+using HanumanInstitute.MvvmDialogs.Avalonia;
 
 namespace AnkiDeckEditor;
 
-public class ViewLocator : IDataTemplate
+public class ViewLocator : StrongViewLocator
 {
-    public Control? Build(object? data)
+    public ViewLocator()
     {
-        if (data is null)
-            return null;
-
-        var name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-        var type = Type.GetType(name);
-
-        if (type == null) return new TextBlock { Text = "Not Found: " + name };
-
-        var control = (Control)Activator.CreateInstance(type)!;
-        control.DataContext = data;
-        return control;
-    }
-
-    public bool Match(object? data)
-    {
-        return data is ViewModelBase;
+        Register<MainWindowViewModel, MainWindow>();
     }
 }
+// public class ViewLocator : IDataTemplate
+// {
+// public Control? Build(object? data)
+// {
+// if (data is null)
+// return null;
+
+// var name = data.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+// var type = Type.GetType(name);
+
+// if (type == null) return new TextBlock { Text = "Not Found: " + name };
+
+// var control = (Control)Activator.CreateInstance(type)!;
+// control.DataContext = data;
+// return control;
+// }
+
+// public bool Match(object? data)
+// {
+// return data is ViewModelBase;
+// }
+// }
