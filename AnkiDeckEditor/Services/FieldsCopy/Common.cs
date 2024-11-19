be8ef52d205648311ed0823ac027d8cbe7dc6fc7
Sergei Markov <smarkov.dev@gmail.com>
2024-11-19 11:22:49 +0200

    Cleanup

--- AnkiDeckEditor/Services/FieldsCopy/Common.cs
+++ AnkiDeckEditor/Services/FieldsCopy/Common.cs
@@ -2,9 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using AnkiDeckEditor.Models;
-using DynamicData;
 
 namespace AnkiDeckEditor.Services.FieldsCopy;
 
@@ -20,16 +18,7 @@ public static class Common
         var resultBuilder = translationData.Select(
                 item => MarkWord((item as ToggleItem)!.Title!, (item as ToggleItem)!.IsChecked))
             .ToList();
-        // var sm = new StringManipulator(string.Join("", resultBuilder).Trim())
-        // .AddSpaseAfterCloseHtmlTag()
-        // .RemoveLeftSpaceFromPunctuation()
-        // .AddSpaceAfterClosePunctuation()
-        // .RemoveRightSpaceClosePunctuation()
-        // .RemoveLeftSpaceClosePunctuation();
-        // var result = FieldTags.TranslationOriginalTemplate.Replace(FieldTags.GetPlaceMarker(1), sm.ResultString);
-
         return JoinWordCollection(resultBuilder);
-        // return result;
     }
 
     public static string ProcessTuple(ValueTuple<string, List<int>> data)
@@ -47,30 +36,10 @@ public static class Common
         }
 
         // Mark word by tag
-
         foreach (var i in data.Item2)
-        {
             resultBuilder[i] = FieldTags.SelectedEntityTemplate.Replace(
-                FieldTags.GetPlaceMarker(1), 
-                resultBuilder[i]); 
-        }
-
-        // var x = new StringManipulator().ClosePunctuationsRegex();
-        
-        // var result = string.Join(" ", resultBuilder);
-
-        // result = new StringManipulator(result).RemoveLeftSpaceClosePunctuation().ResultString;
-        // result = new StringManipulator(result).RemoveRightSpaceClosePunctuation().ResultString;
-        // result = new StringManipulator(result).RemoveLeftSpaceFromPunctuation().ResultString;
-        
-        // List<string> resultBuilder = [..words];
-
-        // If there are no highlighted words, return the string unchanged.
-        // if (!data.Item2.Count.Equals(0))
-            // for (var i = 0; i < words.Length; i++)
-                // resultBuilder[i] = MarkWord(words[i], data.Item2.Contains(i));
-
-        // resultBuilder = words.Select((t, i) => MarkWord(t, data.Item2.Contains(i))).ToList();
+                FieldTags.GetPlaceMarker(1),
+                resultBuilder[i]);
 
         return JoinWordCollection(resultBuilder);
     }
@@ -86,8 +55,6 @@ public static class Common
         var separatedWords = new StringManipulator(value).SeparateLetters(out var punctuationIndexes).ToList();
 
         for (var i = 0; i < separatedWords.Count; i++)
-
-            // indexes - punctuations, not words.
 
             // If this is a word, not a punctuation mark.
             if (!punctuationIndexes.Contains(i))
