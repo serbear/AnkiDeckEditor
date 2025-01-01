d65b451346fdc06cfcd3ea34e50540d5ef94a86a
Sergei Markov <smarkov.dev@gmail.com>
2025-01-01 08:54:11 +0200

    Non verb entity UI design.

--- AnkiDeckEditor/Controls/Tabs/EstonianScreen/NonVerbWordFormsTab.axaml.cs
+++ AnkiDeckEditor/Controls/Tabs/EstonianScreen/NonVerbWordFormsTab.axaml.cs
@@ -1,3 +1,5 @@
+using AnkiDeckEditor.Libs;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 
@@ -8,6 +10,15 @@ public partial class NonVerbWordFormsTab : UserControl
     public NonVerbWordFormsTab()
     {
         InitializeComponent();
+        SizeChanged += OnWindowSizeChanged;
+    }
+
+    private void OnWindowSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        // todo: refact: duplicate code.
+
+        var offset = Common.GetGoldenGoldenRatioOffset(e.NewSize.Height);
+        this.FindControl<StackPanel>("NonVerbWordFormsStackPanel")!.Margin = new Thickness(0, 0, 0, offset);
     }
 
     private void InitializeComponent()
